08a35b2c18efae2ed31c5bd5915a65f0704ab75a
Kevinvazquezg <vgk960401@gmail.com>
2021-09-27 09:58:03 -0500

    Conexión establecida a la base de datos

--- back/db/conexion.js
+++ back/db/conexion.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 
-const sequelize = new Sequelize('tecla', null, null, {
+const sequelize = new Sequelize('demo', null, null, {
   dialect: 'mssql',
   server: process.env.DB_HOST,
   port: process.env.DB_PORT,
